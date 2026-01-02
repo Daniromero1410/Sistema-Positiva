bb3203b8627c6b700afc46f4b97dfbfe3a76d9ac
Daniel Romero ;D <130934263+Daniromero1410@users.noreply.github.com>
2026-01-02 10:00:45 -0500

    Delete backend directory

--- backend/api/deps.py
+++
@@ -1,9 +0,0 @@
-from models.database import SessionLocal
-
-def get_db():
-    """Dependency para obtener sesión de base de datos."""
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
